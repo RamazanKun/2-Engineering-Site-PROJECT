900cecae92f1835966c84cf19a4e8fd2f71062d2
RamazanKun <samonkos10@gmail.com>
2023-04-07 18:10:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const yatay = document.querySelector(".yatay");
+const dikey = document.querySelector("#dikey");
+dikey.addEventListener("click", function(){
+    yatay.classList.toggle("active1");
+})
